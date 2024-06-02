09c8e6724f129fbd8f9330ab1a33d189b8a4cb44
hiriski <hiriski@outlook.com>
2024-06-03 00:48:25 +0700

    feat: add assets & fix xcode config

---
+++ src/assets/images.ts
@@ -0,0 +1,8 @@
+export const ImageAssets = {
+  logo: require('./images/logo.png'),
+  logoXs: require('./images/logo-xs.png'),
+  logoMd: require('./images/logo-md.png'),
+  bgLogo: require('./images/bg-logo.png'),
+  bgLogoXs: require('./images/bg-logo-xs.png'),
+  bgLogoMd: require('./images/bg-logo-md.png'),
+}
