92942ae6fe187594fa6bc3d8e7ac2aa138a7c65c
hiriski <hiriski@outlook.com>
2024-06-04 19:22:20 +0700

    feat: add notification service extension ios

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Riski on 04/06/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
