9b762b63f5d10527b656ddf05a00116234153623
hiriski <hiriski@outlook.com>
2024-10-29 23:30:27 +0700

    feat(playground): added reanimated shake animation transitions

--- src/screens/Playground.screen.tsx
+++ src/screens/Playground.screen.tsx
@@ -1,5 +1,5 @@
 import { RootNavigatorParamList } from '@/navigator/navigator.type'
-import { Text, TouchableOpacity } from 'react-native'
+import { StatusBar, Text, TouchableOpacity } from 'react-native'
 import { View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
@@ -14,7 +14,7 @@ type ListOfPlayground = {
 
 const listOfPlayground: ListOfPlayground[] = [
   {
-    name: 'Playground',
+    name: '(Reanimated) - Snake Animation',
     path: 'shake_animation_screen',
     icon: SolarBoltIcon,
   },
@@ -29,11 +29,26 @@ const PlaygroundScreen = () => {
   }
 
   return (
-    <View style={{ flex: 1, paddingTop: insets.top }}>
-      <Text style={{ textAlign: 'center' }}>Playground Screen</Text>
+    <View style={{ flex: 1 }}>
+      <StatusBar translucent backgroundColor='transparent' barStyle='dark-content' />
+      <View
+        style={{
+          backgroundColor: '#3366FF',
+          paddingTop: insets.top + 4,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingBottom: 12,
+        }}
+      >
+        <Text style={{ textAlign: 'center', fontWeight: 500, fontFamily: 'Jost', color: '#ffffff' }}>Playground</Text>
+      </View>
       <View>
         {listOfPlayground.map(x => (
-          <TouchableOpacity onPress={() => onPress(x.path)} style={{ paddingHorizontal: 24, height: 50 }}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => onPress(x.path)}
+            style={{ paddingHorizontal: 24, paddingVertical: 12, borderBottomColor: '#D5D5D5', borderBottomWidth: 1 }}
+          >
             <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
               {<x.icon height={20} width={20} color='#3366FF' />}
               <Text style={{ fontWeight: '600', marginLeft: 12 }}>{x.name}</Text>
